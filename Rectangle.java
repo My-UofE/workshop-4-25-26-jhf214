6e67df3a99699d8691a94863c3395ad46594ea43
jhf214 <jhf214@exeter.ac.uk>
2026-02-06 12:49:51 +0000

    completed part 3

--- Rectangle.java
+++ Rectangle.java
@@ -4,10 +4,10 @@
 public class Rectangle {
   
   // 4 instance attributes
-  public double width;
-  public double height;
-  public double originX = 0.0;
-  public double originY = 0.0;
+  private double width;
+  private double height;
+  private double originX = 0.0;
+  private double originY = 0.0;
 
   // 1 static attributes
   public static final int NUMBER_OF_SIDES = 4;
@@ -87,6 +87,35 @@ public class Rectangle {
     double ratio = this.calcRatio();
 
     return (0.999 < ratio && ratio < 1.001);
+  }
+
+
+  //getters:
+  public double getWidth() {
+    return this.width;
+  }
+  public double getHeight() {
+    return this.height;
+  }
+  public double getOriginX() {
+    return this.originX;
+  }
+  public double getOriginY() {
+    return this.originY;
+  }
+
+  //setters:
+  public void setWidth(double width) {
+    if (width > 0) { this.width = width; }
+  }
+  public void setHeight(double height) {
+    if (height > 0) { this.height = height; }
+  }
+  public void setOriginX(double originX) {
+    this.originX = originX;
+  }
+  public void setOriginY(double originY) {
+    this.originY = originY;
   }
   
 }
